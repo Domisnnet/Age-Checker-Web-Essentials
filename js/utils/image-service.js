565901a31b2f90b3dc9153389992a4bcb3f83313
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-08 14:45:33 +0000

    corrigido

--- js/utils/image-service.js
+++ js/utils/image-service.js
@@ -1,12 +1,14 @@
-// Função para obter o caminho da imagem, agora como um módulo exportável
+/* =========================
+  Caminho das Imagens
+========================== */
 export function getImagem(idade, sexo) {
   if (idade >= 0 && idade < 10) {
-    return `./src/imagens/foto-bebe-${sexo}.png`;
+    return `./src/images/foto-bebe-${sexo}.png`;
   } else if (idade < 21) {
-    return `./src/imagens/foto-jovem-${sexo}.png`;
+    return `./src/images/foto-jovem-${sexo}.png`;
   } else if (idade < 50) {
-    return `./src/imagens/foto-adulto-${sexo}.png`;
+    return `./src/images/foto-adulto-${sexo}.png`;
   } else {
-    return `./src/imagens/foto-idoso-${sexo}.png`;
+    return `./src/images/foto-idoso-${sexo}.png`;
   }
 }
---
+++ src/js/utils/image-service.js
@@ -0,0 +1,14 @@
+/* =========================
+  Caminho das Imagens
+========================== */
+export function getImagem(idade, sexo) {
+    if (idade >= 0 && idade < 10) {
+      return `./src/images/foto-bebe-${sexo}.png`;
+    } else if (idade < 21) {
+      return `./src/images/foto-jovem-${sexo}.png`;
+    } else if (idade < 50) {
+      return `./src/images/foto-adulto-${sexo}.png`;
+    } else {
+      return `./src/images/foto-idoso-${sexo}.png`;
+    }
+}
